f0444faa6209037333d3221217b9373467f96521
Raihan Alam <152372666+raihannalam@users.noreply.github.com>
2026-06-07 01:19:03 +0530

    Replace API and Firebase credentials with placeholders
    Updated environment configuration to use placeholders for sensitive information.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://172.20.10.3:8080',
-  apiUrl: 'http://172.20.10.3:8080/api/v1',
-  liveKitUrl: 'ws://localhost:7880',
-  firebaseConfig: {
-    apiKey: 'AIzaSyBEYoBHF5NckxkR5v2ZprCkhFlXoFbtb8Y',
-    authDomain: 'verbrixglobal.firebaseapp.com',
-    projectId: 'verbrixglobal',
-    storageBucket: 'verbrixglobal.firebasestorage.app',
-    messagingSenderId: '575093949953',
-    appId: '1:575093949953:web:1357b93b96bad1eef86200',
-    measurementId: 'G-CVT3ZEL857',
-  },
+production: false,
+apiBaseUrl: '<API_BASE_URL>',
+apiUrl: '<API_URL>',
+liveKitUrl: '<LIVEKIT_URL>',
+firebaseConfig: {
+apiKey: '<FIREBASE_API_KEY>',
+authDomain: '<FIREBASE_AUTH_DOMAIN>',
+projectId: '<FIREBASE_PROJECT_ID>',
+storageBucket: '<FIREBASE_STORAGE_BUCKET>',
+messagingSenderId: '<FIREBASE_MESSAGING_SENDER_ID>',
+appId: '<FIREBASE_APP_ID>',
+measurementId: '<FIREBASE_MEASUREMENT_ID>',
+},
 };
