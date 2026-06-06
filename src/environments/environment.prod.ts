23c020cdbb3bebf955eb404ad3a093fc101ba1df
Raihan Alam <152372666+raihannalam@users.noreply.github.com>
2026-06-07 01:03:39 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ export const environment = {
   apiBaseUrl: 'https://api.verbrix.com',
   apiUrl: 'https://api.verbrix.com/api/v1',
   liveKitUrl: 'wss://verbrix-is1gv2zd.livekit.cloud',
-  firebaseConfig: {
-    apiKey: 'AIzaSyBEYoBHF5NckxkR5v2ZprCkhFlXoFbtb8Y',
-    authDomain: 'verbrixglobal.firebaseapp.com',
-    projectId: 'verbrixglobal',
-    storageBucket: 'verbrixglobal.firebasestorage.app',
-    messagingSenderId: '575093949953',
-    appId: '1:575093949953:web:1357b93b96bad1eef86200',
-    measurementId: 'G-CVT3ZEL857',
-  },
+  firebaseConfig = {
+    apiKey: "AIzaSyCEH7-hbaiRc_KxW1xE8_z2r0jEY72UTX8",
+    authDomain: "verbrix-prod.firebaseapp.com",
+    projectId: "verbrix-prod",
+    storageBucket: "verbrix-prod.firebasestorage.app",
+    messagingSenderId: "1060576789167",
+    appId: "1:1060576789167:web:6bc691f281a0d4b4f44a5a",
+    measurementId: "G-49JQVM5HJF"
+};
 };
